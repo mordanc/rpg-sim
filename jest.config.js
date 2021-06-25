14b415407133a1824edfbde708ffe06380941469
mordanc <iusethisacc4games@gmail.com>
2021-06-25 00:52:05 -0400

    fix coverage output

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: './src',
+  rootDir: './',
   modulePathIgnorePatterns: ['build'],
   // This configuration factors to the trails to modules that run a few code to configure or installation the test environment before each test run
   setupFiles: [],
   // This configuration indicates the Jest to the direction to a module that runs some code to configure or installation the testing framework before than each test run
   setupFilesAfterEnv: null,
-  collectCoverageFrom: ['<rootDir>/**/*.ts', '<rootDir>/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '<rootDir>/src/*.ts'],
 };
